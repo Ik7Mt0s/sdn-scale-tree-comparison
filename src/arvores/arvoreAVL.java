c61cfa5ad026358e175c79623a95ebb9ea4b8d57
Ícaro Matos <icaromatoscastelobranco@gmail.com>
2026-05-05 10:17:13 -0300

    criação da classe privada interna No

--- src/arvores/arvoreAVL.java
+++ src/arvores/arvoreAVL.java
@@ -1,5 +1,26 @@
 package arvores;
+import models.PacketRule;
 
 public class arvoreAVL {
     
+    private class No {
+        PacketRule regra;
+        No esquerda, direita;
+        int altura;
+        
+        No(PacketRule regra) {
+            this.regra = regra;
+            this.altura = 1;
+        }
+    }
+    
+    private No raiz;
+    private int contadorRotacoes;
+    private int contadorNos;
+    
+    public arvoreAVL() {
+        raiz = null;
+        contadorRotacoes = 0;
+        contadorNos = 0;
+    }
 }
