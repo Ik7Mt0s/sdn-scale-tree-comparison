c23014b9756974e4fd20fd2cdab1d6e1d8e5d8de
Ícaro Matos <icaromatoscastelobranco@gmail.com>
2026-05-10 18:50:14 -0300

    método de inserção

--- src/arvores/arvoreRBT.java
+++ src/arvores/arvoreRBT.java
@@ -82,4 +82,81 @@ public class arvoreRBT {
 
     private boolean ehVermelho(No no) { return no.cor == VERMELHO; }
     private boolean ehPreto(No no)   { return no.cor == PRETO; }
+
+    public void inserir(PacketRule regra) {
+        No z = new No(regra);
+        No y = NIL;
+        No x = raiz;
+
+        while (x != NIL) {
+            y = x;
+            if (z.regra.compareTo(x.regra) < 0)
+                x = x.esquerda;
+            else
+                x = x.direita;
+        }
+
+        z.pai = y;
+
+        if (y == NIL)
+            raiz = z;
+        else if (z.regra.compareTo(y.regra) < 0)
+            y.esquerda = z;
+        else
+            y.direita = z;
+
+        z.esquerda = NIL;
+        z.direita = NIL;
+        z.cor = VERMELHO;
+
+        contadorNos++;
+
+        corrigirInsercao(z);
+        raiz.cor = PRETO;
+    }
+
+    private void corrigirInsercao(No z) {
+        while (ehVermelho(z.pai)) {
+            No avo = z.pai.pai;
+
+            if (z.pai == avo.esquerda) {
+                No tio = avo.direita;
+
+                if (ehVermelho(tio)) {
+                    z.pai.cor = PRETO;
+                    tio.cor = PRETO;
+                    avo.cor = VERMELHO;
+                    z = avo;
+                } 
+                else {
+                    if (z == z.pai.direita) {
+                        z = z.pai;
+                        rotacionarEsquerda(z);
+                    }
+                    z.pai.cor = PRETO;
+                    avo.cor = VERMELHO;
+                    rotacionarDireita(avo);
+                }
+            } 
+            else {
+                No tio = avo.esquerda;
+
+                if (ehVermelho(tio)) {
+                    z.pai.cor = PRETO;
+                    tio.cor = PRETO;
+                    avo.cor = VERMELHO;
+                    z = avo;
+                } 
+                else {
+                    if (z == z.pai.esquerda) {
+                        z = z.pai;
+                        rotacionarDireita(z);
+                    }
+                    z.pai.cor = PRETO;
+                    avo.cor = VERMELHO;
+                    rotacionarEsquerda(avo);
+                }
+            }
+        }
+    }
 }
