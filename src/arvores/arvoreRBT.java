49a135f0614ccd6addd0c66f727e097c66d0106b
Ícaro Matos <icaromatoscastelobranco@gmail.com>
2026-05-10 18:02:34 -0300

    Classe interna No com sentinela NIL estático

--- src/arvores/arvoreRBT.java
+++ src/arvores/arvoreRBT.java
@@ -4,4 +4,35 @@ import models.PacketRule;
 public class arvoreRBT {
     private static final boolean VERMELHO = true;
     private static final boolean PRETO = false;
+    private static final No NIL = new No(true);
+
+    private static class No {
+        PacketRule regra;
+        No esquerda, direita, pai;
+        boolean cor;
+
+        No(PacketRule regra) {
+            this.regra = regra;
+            this.cor = VERMELHO;
+            this.esquerda = NIL;
+            this.direita = NIL;
+            this.pai = NIL;
+        }
+
+        private No(boolean isNil) {
+            this.regra = null;
+            this.cor = PRETO;
+            this.esquerda = this.direita = this.pai = this;
+        }
+    }
+
+    private No raiz;
+    private int contadorRotacoes;
+    private int contadorNos;
+
+    public arvoreRBT() {
+        raiz = NIL;
+        contadorRotacoes = 0;
+        contadorNos = 0;
+    }
 }
